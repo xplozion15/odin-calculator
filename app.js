d40141ccfc3fdd86b7ebccc0c193fb727aedffca
Abhilash Avinash Patil <89824888+abhilash15500@users.noreply.github.com>
2024-06-09 18:50:10 +0530

    make a function operate that calls  addition sub mul div function on the numbers

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+let num1 = 5;
+let num2 = 10;
+let operator = "/"
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -14,5 +18,28 @@ function multiply(num1, num2) {
 
 
 function divide(num1, num2) {
-    return num1 * num2;
+    return num1 / num2;
 }
+
+
+
+function operate(num1,num2,operator) {
+    if(operator == "+") {
+        return add(num1,num2);
+    }
+
+    else if (operator == "-") {
+        return subtract(num1,num2);
+    }
+
+    else if (operator == "*") {
+        return multiply(num1,num2);
+    }
+
+    else if (operator == "/") {
+        return divide(num1,num2);
+    }
+}
+
+
+console.log(operate(num1,num2,operator))
